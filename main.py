68c6692ae10370881ab03bf296f663e5fca285ae
MrLucasFischer <lucasfischerpt@gmail.com>
2018-12-27 16:33:59 +0000

    updating docs

--- main.py
+++ main.py
@@ -43,7 +43,13 @@ filename = "./data/sorted_data.csv"
 
 def create_row(line):
     """
-        Add doc
+        Function that creates a structured tuple representing a row in a RDD
+
+        Params:
+            line - A line from the input file
+
+        Rerturns:
+            A Strcutured tuple with 14 positions
     """
     #Field - Array_position
 
@@ -67,7 +73,13 @@ def create_row(line):
 
 def create_row_df(line):
     """
-        Add doc
+        Function that creates a Structured Row object representing a Row in a DataFrame
+
+        Params:
+            line - A line from the input file
+
+        Returns:
+            A Row object representing a row in a Dataframe
     """
     #Field - Array_position
 
@@ -92,7 +104,13 @@ def create_row_df(line):
 
 def filter_lines(line):
     """
-        Add doc
+        Function that filters out empty lines as well as lines that have coordinates as 0.0000 (non relevant points)
+
+        Params:
+            line - A line from the input file
+
+        Returns:
+            True if the line passed this condition, False otherwise
     """
     splitted_line = line.split(',')
     return (len(line) > 0) and (float(splitted_line[6]) != 0) and (float(splitted_line[8]) != 0)
@@ -101,7 +119,14 @@ def filter_lines(line):
 
 def estimate_cellid(lat, lon):
     """
-        Add doc
+        Function that estimates a cell ID given a latitude and longitude based on the coordinates of cell 1.1
+
+        Params:
+            lat - Input latitude for which to find the cellID
+            lon - Input longitude for which to fin the cellID
+
+        Returns:
+            A String such as 'xxx.xxx' representing the ID of the cell
     """
     x0 = -74.913585 #longitude of cell 1.1
     y0 = 41.474937  #latitude of cell 1.1
@@ -117,40 +142,61 @@ def estimate_cellid(lat, lon):
 
 
 
-def create_key_value(line):
+def create_key_value(structured_tuple):
     """
-        Add doc
+        Function that from a structured tuple organizes it into a Key-Value formation.
+        The key is a tuple containing both the weekday and the hour.
+        The value is a dictionary containing only one item, this dictionary is to be merged on the reducer.
+
+        Params:
+            structured_tuple - A tuple representing a line of the input file
+
+        Returns:
+            A tuple organized into a Key-Value formation
     """
 
-    weekday = convert_to_weekday(line[0])
-    hour = convert_to_hour(line[0])
-    route = f"{line[11]}-{line[12]}"
+    weekday = convert_to_weekday(structured_tuple[0])
+    hour = convert_to_hour(structured_tuple[0])
+    route = f"{structured_tuple[11]}-{structured_tuple[12]}"
 
     return ((weekday, hour), {route: 1})
 
 
-def create_key_value_query2(line):
-    """
-        Add doc
-    """
-    weekday = convert_to_weekday(line[0])
-    hour = convert_to_hour(line[0])
-    pass
-
 
 def custom_reducer(accum, elem):
+    """
+        Custom function to be used in reduceByKey.
+        This function well merge dictionaries counting the number of times each time appears
+
+        Params:
+            accum - An accumulator dictionary
+            elem - The dictionary of the current iteration
+
+        Returns:
+            The accumulator dictionary updated with information obtained by elem
+    """
+
+    #store the only existing item inside elem
     key, value = elem.popitem()
     
-    if(key in accum):
+    if(key in accum): #If accum already has this key, then update its value
         accum[key] += value
-    else:
+    else:   #If accum does not have this key, add it
         accum[key] = value
 
     return accum
 
+
+
 def convert_to_weekday(date):
     """
         Function that converts a date to weekday
+
+        Params:
+            date - Unix timestamp formatted date in string form
+
+        Returns:
+            A string with the weekday of the input date
     """
     date_obj = dt.strptime(date, '%Y-%m-%d %H:%M:%S')
     return (calendar.day_name[date_obj.weekday()]).lower()
@@ -160,21 +206,39 @@ def convert_to_weekday(date):
 def convert_to_hour(date):
     """
         Function that gets the hour from a date
+
+        Params:
+            date - Unix timestamp formatted date in string form
+
+        Returns:
+            The hour portion of the input date
     """
     return date[11:13]
 
 
 
-def filter_outliers(line):
+def filter_outliers(structured_tuple):
     """
-        Add doc
+        Function that filters out outlier cells. Cells whos ID is above 300 are considered outliers since
+        the grid only extends to cell 300.300
+
+        Params:
+            structured_tuple - A tuple containing information of a line in the RDD
+
+        Returns:
+            True if there are no outlier cells in the input tuple, False otherwise
     """
-    pickup_cell_x , pickup_cell_y = line[11].split(".")
-    dropoff_cell_x , dropoff_cell_y = line[12].split(".")
+    pickup_cell_x , pickup_cell_y = structured_tuple[11].split(".")
+    dropoff_cell_x , dropoff_cell_y = structured_tuple[12].split(".")
     return (float(pickup_cell_x) <= 300) and (float(pickup_cell_y) <= 300) and (float(dropoff_cell_x) <= 300) and (float(dropoff_cell_y) <= 300)
+
+
 
 def query1():
     try:
+        
+        #timestamp to mesure the time taken
+        time_before = dt.now()
 
         #read csv file (change this to the full dataset instead of just the sample)
         raw_data = sc.textFile(filename)
@@ -191,12 +255,22 @@ def query1():
         # ((weekday, hour), {route})
         organized_lines = filtered_rdd.map(lambda line : create_key_value(line))
 
+        #Group all values by its key, reducing them acording to custom_reducer
         grouped = organized_lines.reduceByKey(lambda accum, elem: custom_reducer(accum, elem))
 
+        #Sort descendingly the dictionaries present in the values and take only the first 10 elements
         top_routes = grouped.mapValues(lambda route_dict: sorted(route_dict, key = route_dict.get, reverse = True)[:10])
 
-        for a in top_routes.take(1000):
+        #Store the retrieved results
+        top_routes.saveAsTextFile("spark_rdd_results/query1")
+
+        for a in top_routes.take(2):
             print(a)
+
+        time_after = dt.now()
+        seconds = (time_after - time_before).total_seconds()
+        print("Execution time {} seconds".format(seconds))
+
 
         sc.stop()
     except:
@@ -207,6 +281,13 @@ def query1():
 
 def query2():
     try:
+
+        #timestamp to mesure the time taken
+        time_before = dt.now()
+
+        # convert_to_weekday_udf = udf(lambda pickup_date: convert_to_weekday(pickup_date), StringType())
+        convert_to_weekday_udf = udf(lambda pickup_date: convert_to_weekday(pickup_date), StringType())
+        convert_to_hour_udf = udf(lambda pickup_date: pickup_date[11:13], StringType())
 
         #read csv file (change this to the full dataset instead of just the sample)
         raw_data = sc.textFile(filename)
@@ -226,10 +307,10 @@ def query2():
         # Get the dropoffs of the last 15 minutes for each cell
         # get the average of the fare
         profit_by_area_15min = filtered_df \
-            .groupBy(window("dropoff_dt", "900 seconds"), "pickup_cell") \
+            .groupBy(window("dropoff_dt", "900 seconds"), convert_to_weekday_udf("pickup_dt").alias("weekday"), convert_to_hour_udf("pickup_dt").alias("hour"), "pickup_cell") \
             .agg(avg(filtered_df.fare_amount + filtered_df.tip_amount).alias("median_fare")) \
             .orderBy("median_fare", ascending = False) \
-            .select("pickup_cell" ,"median_fare")
+            .select("weekday", "hour", "pickup_cell")
 
 
         # empty_taxis = filtered_df \
@@ -237,7 +318,13 @@ def query2():
         #     .agg(countDistinct("taxi_id").alias("empty_taxis")) \
         #     .select("dropoff_cell", "empty_taxis")
 
-        profit_by_area_15min.show(10)
+        profit_by_area_15min.show(2)
+        
+        profit_by_area_15min.rdd.map(lambda row: ((row.weekday, row.hour), row.pickup_cell)).saveAsTextFile("spark_rdd_results/query2")
+        
+        time_after = dt.now()
+        seconds = (time_after - time_before).total_seconds()
+        print("Execution time {} seconds".format(seconds))
 
         sc.stop()
     except:
@@ -248,10 +335,8 @@ def query2():
 
 
 
+
 def query3():
-    """
-        Add doc
-    """
     schema = StructType([
         StructField("medallion", StringType()),
         StructField("hack_license", StringType()),
@@ -271,20 +356,26 @@ def query3():
         StructField("tolls_amount", FloatType()),
         StructField("total_amount", FloatType())
     ])
-    
+
     # Load and parse the data
     data = spark.read.schema(schema).option("header", "false").csv("./data/sorted_data.csv")
 
+    #Define the target columns and output column
     assembler = VectorAssembler(
         inputCols = ["pickup_latitude", "pickup_longitude"],
         outputCol = "features"
         )
 
+    #Transform the data according to the Assembler created above
     data_prepared = assembler.transform(data)
 
+    #Class used to evaluate the clusters
     evaluator = ClusteringEvaluator()
 
-    for i in range(11, 52, 4): #find other k values
+
+    for i in [5, 31, 75]: #find other k values
+        
+        #Instanciate Kmeans class with the given K value
         kmeans = KMeans(k = i)
 
         #Fit the data
